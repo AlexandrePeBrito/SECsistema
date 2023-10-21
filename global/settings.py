6fd44d476a18b3003c908ac0ce3bb3166cf17ab9
Alexandre Brito <alexandre_pereirabrito@yahoo.com.br>
2023-10-21 09:59:04 -0300

    alterando o host mysql para o ip

--- global/settings.py
+++ global/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
         "NAME": "dbsec",
         "USER": "root",
         "PASSWORD": "123456",
-        "HOST": "host.docker.internal",
+        "HOST": "127.0.0.1",
         "PORT": "3306",
     },
 }
