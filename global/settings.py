4d8f552fcce6ccd8ef9abab5a41af46b742eef16
Alexandre Brito <alexandre_pereirabrito@yahoo.com.br>
2023-10-21 10:03:03 -0300

    para push

--- global/settings.py
+++ global/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
         "NAME": "dbsec",
         "USER": "root",
         "PASSWORD": "123456",
-        "HOST": "127.0.0.1",
+        "HOST": "127.0.0.1", 
         "PORT": "3306",
     },
 }
